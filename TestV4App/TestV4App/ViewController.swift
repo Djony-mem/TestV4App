5e4706c0f0bb889d513aa335a33ad7847e3dc631
Eugeniya <bruyko.e@gmai.com>
2023-09-26 11:29:01 -0600

    Добавили проект

---
+++ TestV4App/TestV4App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestV4App
+//
+//  Created by brubru on 26.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
